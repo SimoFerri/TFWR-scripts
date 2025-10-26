529f8a48afdd8de620ca2c4b042e36217acf8214
Simone Ferri <simone1.ferri@mail.polimi.it>
2025-10-26 17:14:15 +0100

    parallel Pumpkin production

---
+++ parallel_pumpkins.py
@@ -0,0 +1,62 @@
+# Author: Simone Ferri
+# Date: 26/10/2025
+
+# This script includes the functions that handle the planting and the harvesting
+# of pumpkins
+import utils
+
+# This plants all the Pumpkins in a column, this is executed by a "Worker Drone"
+def plant_pump_col():
+	for _ in range(get_world_size()):
+		harvest()
+		if get_ground_type() != Grounds.Soil:
+			till()
+		plant(Entities.Pumpkin)
+
+		if num_items(Items.Water) > 0 and get_water() < 0.5:
+			use_item(Items.Water)
+
+		move(North)
+
+# This plants Pumpkins all across the farm
+def plant_pump():
+	utils.set_position(0, 0)
+	for _ in range(get_world_size()):
+		while num_drones() >= max_drones():
+			continue
+		spawn_drone(plant_pump_col)
+		move(East)
+		
+	while num_drones() > 1:
+		continue
+
+# This replace all Dead_Pumpkins with a Pumpkin in a column
+def remove_dead_pump_col():
+	for _ in range(get_world_size()):
+		while not can_harvest():
+			if get_entity_type() == Entities.Dead_Pumpkin:
+				plant(Entities.Pumpkin)
+
+		move(North)
+			
+
+# This replace all the Dead_Pumpkins with Pumpkins by re-planting them, this leads to the
+# generation of the biggest possible pumpking for the farm dimensions
+def remove_dead_pump():
+	utils.set_position(0, 0)
+	for _ in range(get_world_size()):
+		while num_drones() >= max_drones():
+			continue
+		spawn_drone(remove_dead_pump_col)
+		move(East)
+		
+	while num_drones() > 1:
+		continue
+
+# This wraps up the Pumpkin production
+def produce_pump():
+	change_hat(Hats.Purple_Hat)
+	plant_pump()
+	remove_dead_pump()
+	harvest()
+	
